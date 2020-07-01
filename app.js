1e3a7994eb6e0035ecf4ac8ac1ab96fd6037168a
MyanPatel98 <myanpatel98@gmail.com>
2020-07-01 17:07:11 +0100

    Functional ToDo list Web application without a database

---
+++ app.js
@@ -0,0 +1,63 @@
+// jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+let items = ["Buy food", "Cook Food", "Eat Food"];
+let workItems = [];
+
+app.get("/", function(req, res){
+
+    let day = date.getDate();
+    res.render('list', {listTitle: day, newListItems: items});
+});
+
+app.post('/', function(req, res){
+    // Retrieving the user input
+    let item = req.body.newItem;
+
+    // Check if the request came from the work list or main list
+    // The value 'Work' is the value of the key 'list'
+    if (req.body.list === 'Work'){
+        // if work list, push the user input to the work list
+        workItems.push(item);
+        // re-direct to the work route (app.post('/work')) to render the work list page
+        res.redirect("/work")
+    }else{
+        // if false then request must be from the main list
+        // in which case add the user input to the main list
+        items.push(item);
+        // re-direct to the home route to render the page with the date-title etc
+        res.redirect("/");
+    }
+});
+
+app.get('/work', function(req,res){
+    
+    res.render('list',{listTitle: "Work List", newListItems: workItems});
+
+});
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+
+app.get("/about", function(req,res){
+    res.render('about');
+});
+
+app.post('/about', function(req, res){
+    res.redirect("/about");
+});
+
+app.listen(3000, function(){
+    console.log("server running on port 3000");
+})
